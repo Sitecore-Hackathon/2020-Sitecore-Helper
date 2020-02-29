5556eb3ecb2e5c5775b7d87f12d33ad9afe2a07d
Himanshu Agarwal <Himanshu.agarwal@valtech.com>
2020-03-01 00:18:54 +0530

    header and footer

---
+++ src/Feature/Navigation/code/Controllers/NavigationController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hackathon.Feature.Navigation.Controllers
+{
+    public class NavigationController : Controller
+    {
+        // GET: Navigation
+        public ActionResult Header()
+        {
+            return View();
+        } public ActionResult Footer()
+        {
+            return View();
+        }
+    }
+}
